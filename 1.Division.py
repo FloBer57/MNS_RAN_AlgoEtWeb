79c2fc708815a3737bfd0e3a6fafe7ca53fa8eea
Flober57 <florent.bernar@outlook.fr>
2024-01-25 11:54:13 +0100

    Upload des données

---
+++ 1.Division.py
@@ -0,0 +1,8 @@
+print("Ceci est un petit script python pour diviser 2 nombres")
+
+a = float(input("Veuillez rentrer un nombre : (Appuyez sur 'entrée' pour continuer) "))
+b = float(input("Veuillez rentrer un nombre : (Appuyez sur 'entrée' pour continuer) "))
+
+somme = a / b 
+
+print("le résultat est",somme)
